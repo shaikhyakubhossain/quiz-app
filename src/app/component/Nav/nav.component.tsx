6c84383202712e110d8e7ce94a8308d153d41aee
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-21 08:48:43 +0530

    Add nav component

---
+++ src/app/component/Nav/nav.component.tsx
@@ -0,0 +1,10 @@
+import styles from './nav.module.scss';
+
+
+export default function Nav() {
+    return (
+        <div className={`${styles.mainContainer} flex`}>
+            <div></div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import './globals.css';
+import Nav from './component/Nav/nav.component';
 
 export default function Home(): JSX.Element{
   return (
       <div className=''>
-        hi
+        <Nav />
       </div>
   )
 }
