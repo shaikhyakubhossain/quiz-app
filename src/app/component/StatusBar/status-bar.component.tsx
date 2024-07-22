bbf81ea48678807e782edd2f77e670a6782f8ad6
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-22 10:43:38 +0530

    Update StatusBar component

--- src/app/component/StatusBar/status-bar.component.tsx
+++ src/app/component/StatusBar/status-bar.component.tsx
@@ -3,10 +3,10 @@ import styles from './status-bar.module.scss';
 
 export default function StatusBar() {
     return (
-        <div className={`${styles.mainContainer} flex justify-between w-4/5 mx-auto border-b-4 border-black`}>
-            <div>Q 01/10</div>
-            <div>100 points</div>
-            <div>00:00 T</div>
+        <div className={`${styles.mainContainer} flex justify-between w-4/5 mx-auto mt-10 pb-3 border-b-4 border-black font-bold`}>
+            <div className={`${styles.questionCount}`}>Q 01/10</div>
+            <div className={`${styles.pointCount}`}>100 points</div>
+            <div className={`${styles.timeCount}`}>00:00 T</div>
         </div>
     );
 }
