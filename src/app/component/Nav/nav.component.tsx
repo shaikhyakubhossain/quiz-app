81e6479a024c4382fd89ea332e0db421329ca8bb
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-22 10:49:26 +0530

    Update css

--- src/app/component/Nav/nav.component.tsx
+++ src/app/component/Nav/nav.component.tsx
@@ -4,7 +4,7 @@ import Btn from '../Btn/btn.component';
 
 export default function Nav() {
     return (
-        <div className={`${styles.mainContainer} flex w-full h-28 border-b-4 border-black`}>
+        <div className={`${styles.mainContainer} flex w-full h-28 border-b-4 border-black bg-white`}>
             <div className={`${styles.menuBtn}`}></div>
             <Btn>test</Btn>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import StatusBar from './component/StatusBar/status-bar.component';
 
 export default function Home(): JSX.Element{
   return (
-      <div className=''>
+      <div className=' bg-orange-100'>
         <Nav />
         <StatusBar />
       </div>
