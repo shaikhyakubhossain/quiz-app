7e3d9005555189cf14695b7917d027d0ca339107
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-22 06:20:05 +0530

    Update nav component

--- src/app/component/Nav/nav.component.tsx
+++ src/app/component/Nav/nav.component.tsx
@@ -3,8 +3,8 @@ import styles from './nav.module.scss';
 
 export default function Nav() {
     return (
-        <div className={`${styles.mainContainer} flex`}>
-            <div></div>
+        <div className={`${styles.mainContainer} flex w-full h-16 border-b-4 border-black`}>
+            <div className={`${styles.menuBtn}`}></div>
         </div>
     );
 }
