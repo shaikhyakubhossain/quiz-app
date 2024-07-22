8cf75ad8bba0fd6b68af67681a52bf7619eab9de
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-22 09:08:45 +0530

    Update component css

--- src/app/component/Btn/btn.component.tsx
+++ src/app/component/Btn/btn.component.tsx
@@ -9,6 +9,6 @@ type PropsType = {
 
 export default function Btn(props: PropsType): JSX.Element {
     return (
-        <div className={`${styles.mainContainer} p-3 bg-yellow-500 `}>{props.children}</div>
+        <div className={`${styles.mainContainer} my-auto px-6 py-2 bg-yellow-500 rounded`}>{props.children}</div>
     );
 }
--- src/app/component/Nav/nav.component.tsx
+++ src/app/component/Nav/nav.component.tsx
@@ -4,9 +4,9 @@ import Btn from '../Btn/btn.component';
 
 export default function Nav() {
     return (
-        <div className={`${styles.mainContainer} flex w-full h-16 border-b-4 border-black`}>
+        <div className={`${styles.mainContainer} flex w-full h-28 border-b-4 border-black`}>
             <div className={`${styles.menuBtn}`}></div>
-            <div><Btn>test</Btn></div>
+            <Btn>test</Btn>
         </div>
     );
 }
