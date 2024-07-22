12a5c04bd58f91ce556dd37869fc71756a45b53e
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-22 10:56:39 +0530

    Add Question component

---
+++ src/app/component/Question/question.component.tsx
@@ -0,0 +1,12 @@
+import styles from './question.module.scss';
+
+
+
+export default function Question(): JSX.Element {
+
+    return (
+        <div className={`${styles.mainContainer} `}>
+            testing - question
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import './globals.css';
 import Nav from './component/Nav/nav.component';
 import StatusBar from './component/StatusBar/status-bar.component';
+import Question from './component/Question/question.component';
 
 export default function Home(): JSX.Element{
   return (
       <div className=' bg-orange-100'>
         <Nav />
         <StatusBar />
+        <Question />
       </div>
   )
 }
