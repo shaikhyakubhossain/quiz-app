6a6de0773b25f405b8db3ffed8d6ae3ccf6f7659
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-23 10:02:18 +0530

    Fetch api

--- src/app/component/HeroContent/hero-content.component.tsx
+++ src/app/component/HeroContent/hero-content.component.tsx
@@ -2,7 +2,21 @@ import styles from './hero-content.module.scss';
 import StatusBar from '../StatusBar/status-bar.component';
 import Question from '../Question/question.component';
 
-export default function HeroContent(): JSX.Element {
+``
+type apiDataType = {
+    type: string,
+    difficulty: string,
+    category: string,
+    question: string,
+    correct_answer: string,
+    incorrect_answers: string[]
+  }
+
+type propsType = {
+    apiData: apiDataType[]
+}
+
+export default function HeroContent(props: propsType): JSX.Element {
     return (
         <div className={`${styles.mainContainer}`}>
             <StatusBar />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@ import './globals.css';
 import Nav from './component/Nav/nav.component';
 import HeroContent from './component/HeroContent/hero-content.component';
 
-export default function Home(): JSX.Element{
+export default async function Home(): Promise<JSX.Element> {
+
+  const response = await fetch('https://opentdb.com/api.php?amount=10&category=27');
+
+  const json = await response.json();
+
   return (
       <div className=' bg-orange-100'>
         <Nav />
-        <HeroContent />
+        <HeroContent apiData={json.result} />
       </div>
   )
 }
