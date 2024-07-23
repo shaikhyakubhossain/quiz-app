ae98f2251cd672c6cd0bead21f9661e0efce80f1
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-24 05:14:19 +0530

    Improve Btn css

--- src/app/component/Btn/btn.component.tsx
+++ src/app/component/Btn/btn.component.tsx
@@ -9,6 +9,6 @@ type PropsType = {
 
 export default function Btn(props: PropsType): JSX.Element {
     return (
-        <span className={`${styles.mainContainer} my-auto mx-6 px-6 py-2 rounded`}>{props.children}</span>
+        <span className={`${styles.mainContainer} my-auto mx-6 px-6 py-2 rounded font-medium text-2xl`}>{props.children}</span>
     );
 }
