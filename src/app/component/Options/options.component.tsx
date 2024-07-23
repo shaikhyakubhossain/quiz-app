7bc4cf619090ae3d025b4410a11b1953f2379eed
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-23 09:46:48 +0530

    Add Options

---
+++ src/app/component/Options/options.component.tsx
@@ -0,0 +1,9 @@
+import styles from './option.module.scss';
+
+export default function Options(): JSX.Element {
+    return (
+        <div className={`${styles.mainContainer} `}>
+            
+        </div>
+    )
+}
