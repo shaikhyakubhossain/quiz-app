2e2d1c959756fa9558ed6283660de92abf6639f7
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-24 23:19:08 +0530

    Update Options and Btn component

--- src/app/component/Btn/btn.component.tsx
+++ src/app/component/Btn/btn.component.tsx
@@ -1,15 +1,12 @@
 import styles from './btn.module.scss';
 
-
-
-
 type PropsType = {
-    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+    onClick?: (event: React.MouseEvent<HTMLSpanElement>) => void
     children: React.ReactNode
 }
 
 export default function Btn(props: PropsType): JSX.Element {
     return (
-        <span className={`${styles.mainContainer} my-auto mx-6 px-6 py-2 rounded font-medium text-2xl`}>{props.children}</span>
+        <span className={`${styles.mainContainer} my-auto mx-6 px-6 py-2 rounded font-medium text-2xl`} onClick={props.onClick}>{props.children}</span>
     );
 }
--- src/app/component/Options/options.component.tsx
+++ src/app/component/Options/options.component.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState, useEffect } from 'react';
 import styles from './options.module.scss';
 import { shuffleOptions } from '../../constants/Utils/methods';
 import Btn from '../Btn/btn.component';
@@ -13,16 +15,43 @@ type propsType = {
 
 export default function Options(props: propsType): JSX.Element {
 
-    const options = props.incorrect_answers;
-    options.push(props.correct_answer);
+    const [options, setOptions] = useState<string[] | null>(null);
+    const correctColor = "rgb(64, 255, 96)";
+    const incorrectColor = "rgb(255, 64, 64)";
+    // const options = props.incorrect_answers;
+    // options.push(props.correct_answer);
+
+    useEffect(() => {
+        setOptions([...props.incorrect_answers, props.correct_answer]);
+    },[]);
+
+
+    const checkAnswer = (event: React.MouseEvent<HTMLSpanElement>) => {
+        const target = event.target as HTMLSpanElement;
+        const initialColor = target.style.backgroundColor;
+
+        target.textContent === props.correct_answer ? target.style.backgroundColor = correctColor : target.style.backgroundColor = incorrectColor;
+        
+        setTimeout(() => {
+            target.style.backgroundColor = initialColor;
+        }, 1000);
+
+    }
+
 
     return (
-        <div className={`${styles.mainContainer} flex flex-wrap justify-center mt-10`}>
-            {
-                shuffleOptions(options).map((item, index): JSX.Element => {
-                    return <Btn key={index} children={item} />
-                })  
-            }
+        <div className={`${styles.mainContainer} mt-10`}>
+            <div className={`${styles.optionsContainer} flex flex-wrap justify-center`}>
+                {
+                    options && shuffleOptions(options).map((item, index): JSX.Element => {
+                        return (
+                            <>
+                                <Btn key={index} onClick={checkAnswer} children={item} />
+                            </>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
