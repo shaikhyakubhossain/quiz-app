d7226e5556e090c1fd31b9622a6fec3d538ff00c
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-24 05:43:25 +0530

    Update Options component

--- src/app/component/Options/options.component.tsx
+++ src/app/component/Options/options.component.tsx
@@ -1,4 +1,6 @@
 import styles from './options.module.scss';
+import { shuffleOptions } from '../../constants/Utils/methods';
+import Btn from '../Btn/btn.component';
 
 
 
@@ -10,9 +12,17 @@ type propsType = {
 
 
 export default function Options(props: propsType): JSX.Element {
+
+    const options = props.incorrect_answers;
+    options.push(props.correct_answer);
+
     return (
-        <div className={`${styles.mainContainer} `}>
-            
+        <div className={`${styles.mainContainer} flex flex-wrap justify-center mt-10`}>
+            {
+                shuffleOptions(options).map((item, index): JSX.Element => {
+                    return <Btn key={index} children={item} />
+                })  
+            }
         </div>
     )
 }
