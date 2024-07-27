ecfac604043314f7b313854a0a601c60b12e5f8f
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-28 02:23:53 +0530

    Configure redux-persist

--- src/app/component/RTKStoreProvider/rtk-store-provider.component.tsx
+++ src/app/component/RTKStoreProvider/rtk-store-provider.component.tsx
@@ -1,14 +1,24 @@
 'use client';
-import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { store } from '../../../lib/store'
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 export default function RTKStoreProvider({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const persistor = persistStore(store)
   
 
-  return <Provider store={store}>{children}</Provider>
+  return (
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+    {children}
+    </PersistGate>
+  </Provider>
+  )
+  
 }
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,12 +2,27 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { mainTimerSlice } from './features/mainTimer/mainTimerSlice';
 import { pointsCounterSlice } from './features/pointsCounter/pointsCounterSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { combineReducers } from '@reduxjs/toolkit';
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage: storage
+}
+
+const reducer = combineReducers({
+    mainTimer: mainTimerSlice.reducer,
+    pointsCounter: pointsCounterSlice.reducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+
 
 export const store = configureStore({
-    reducer: {
-        mainTimer: mainTimerSlice.reducer,
-        pointsCounter: pointsCounterSlice.reducer
-    }
+    reducer: persistedReducer
 })
 
 export type RootState = ReturnType<typeof store.getState>
