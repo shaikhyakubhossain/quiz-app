da27a4d9b86a216ac252c2e898925d565c3d2eeb
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-29 16:27:21 +0530

    Add dynamic Btn css: bgColor change on click in QuestionSettings component

--- src/app/component/QuestionSettings/question-settings.component.tsx
+++ src/app/component/QuestionSettings/question-settings.component.tsx
@@ -8,7 +8,30 @@ type propsType = {
 
 }
 
+
+let categoryBtnClickPrev: HTMLSpanElement | null = null;
+let difficultyBtnClickPrev: HTMLSpanElement | null = null;
+
 export default function QuestionSettings(props: propsType): JSX.Element {
+
+    const handleBtnColorChangeOnClick = (event: React.MouseEvent<HTMLSpanElement>, btnType: string) => {
+        const btnClickCurrent = event.target as HTMLSpanElement;
+        const btnInitialColor = btnClickCurrent.style.backgroundColor;
+        
+        if(btnType === "category") {
+            categoryBtnClickPrev  ? categoryBtnClickPrev.style.backgroundColor = btnInitialColor : null;
+            btnClickCurrent.style.backgroundColor = '#FF8383';
+            categoryBtnClickPrev = btnClickCurrent;
+        }
+        else {
+            difficultyBtnClickPrev  ? difficultyBtnClickPrev.style.backgroundColor = btnInitialColor : null;
+            btnClickCurrent.style.backgroundColor = '#FF8383';
+            difficultyBtnClickPrev = btnClickCurrent;
+        }
+
+
+    }
+
     return (
         <div className={`${styles.mainContainer}`}>
             <div className={`text-3xl`}>Select Category</div>
@@ -16,16 +39,28 @@ export default function QuestionSettings(props: propsType): JSX.Element {
                 {
                     categoriesList.map((item, index): JSX.Element => {
                         return(
-                            <div key={index}><Btn customTW="text-sm" onClick={() => props.setCategory('category=' + item.value)}>{item.title}</Btn></div>
+                            <div key={index}><Btn customTW="text-sm" onClick={(e) => {
+                                props.setCategory('category=' + item.value);
+                                handleBtnColorChangeOnClick(e, "category");
+                            }}>{item.title}</Btn></div>
                         )
                     })
                 }
             </div>
             <div className={`text-3xl`}>Select Difficulty</div>
             <div className={`${styles.difficultyContainer} flex justify-center overflow-hidden mx-auto mb-4`}>
-                    <div><Btn customTW="text-sm" onClick={() => props.setDifficulty('difficulty=easy')}>Easy</Btn></div>
-                    <div><Btn customTW="text-sm" onClick={() => props.setDifficulty('difficulty=medium')}>Normal</Btn></div>
-                    <div><Btn customTW="text-sm" onClick={() => props.setDifficulty('difficulty=hard')}>Hard</Btn></div>
+                    <div><Btn customTW="text-sm" onClick={(e) => {
+                        props.setDifficulty('difficulty=easy')
+                        handleBtnColorChangeOnClick(e, "difficulty")
+                        }}>Easy</Btn></div>
+                    <div><Btn customTW="text-sm" onClick={(e) => {
+                        props.setDifficulty('difficulty=medium')
+                        handleBtnColorChangeOnClick(e, "difficulty")
+                        }}>Normal</Btn></div>
+                    <div><Btn customTW="text-sm" onClick={(e) => {
+                        props.setDifficulty('difficulty=hard')
+                        handleBtnColorChangeOnClick(e, "difficulty")
+                        }}>Hard</Btn></div>
             </div>
         </div>
     )
