ec2d79c799d018a45bb9a49ba1bd710223b4e52e
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-29 14:35:14 +0530

    Update Nav component

--- src/app/component/Nav/nav.component.tsx
+++ src/app/component/Nav/nav.component.tsx
@@ -1,12 +1,11 @@
 import styles from './nav.module.scss';
-import Btn from '../Btn/btn.component';
-
+import ResetStatesAndRouteChangeBtn from '../ResetStatesAndRouteChangeBtn/reset-states-and-route-change-btn.component';
 
 export default function Nav() {
     return (
         <div className={`${styles.mainContainer} flex w-full h-28 border-b-4 border-black bg-white`}>
             <div className={`${styles.menuBtn}`}></div>
-            <Btn>testing</Btn>
+            <ResetStatesAndRouteChangeBtn url={'/'} customBgColor='#FF58FA'>Home</ResetStatesAndRouteChangeBtn>
         </div>
     );
 }
