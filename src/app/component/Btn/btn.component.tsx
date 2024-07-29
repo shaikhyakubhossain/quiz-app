70a06192c3fbc4a271347f5b44dfc76fd5a1e909
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-07-29 14:15:19 +0530

    Improve Btn component, Update QuestionSettings css

--- src/app/component/Btn/btn.component.tsx
+++ src/app/component/Btn/btn.component.tsx
@@ -1,12 +1,15 @@
 import styles from './btn.module.scss';
 
 type PropsType = {
-    onClick?: (event: React.MouseEvent<HTMLSpanElement>) => void
-    children: React.ReactNode
+    onClick?: (event: React.MouseEvent<HTMLSpanElement>) => void,
+    children: React.ReactNode,
+    customTW?: string,
+    customBgColor?: string
 }
 
 export default function Btn(props: PropsType): JSX.Element {
+
     return (
-        <span className={`${styles.mainContainer} ${styles.whenActive} my-6 mx-6 px-6 py-2 rounded font-medium text-2xl`} onClick={props.onClick}>{props.children}</span>
+        <span className={`${styles.mainContainer} ${styles.whenActive} my-6 mx-6 px-6 py-2 rounded font-medium text-2xl ` + props.customTW} style={{backgroundColor: props.customBgColor}} onClick={props.onClick}>{props.children}</span>
     );
 }
--- src/app/component/QuestionSettings/question-settings.component.tsx
+++ src/app/component/QuestionSettings/question-settings.component.tsx
@@ -12,20 +12,20 @@ export default function QuestionSettings(props: propsType): JSX.Element {
     return (
         <div className={`${styles.mainContainer}`}>
             <div className={`text-3xl`}>Select Category</div>
-            <div className={`${styles.categoryContainer} flex justify-center overflow-hidden mx-auto`}>
+            <div className={`${styles.categoryContainer} flex justify-center flex-wrap `}>
                 {
                     categoriesList.map((item, index): JSX.Element => {
                         return(
-                            <div key={index}><Btn onClick={() => props.setCategory('category=' + item.value)}>{item.title}</Btn></div>
+                            <div key={index}><Btn customTW="text-sm" onClick={() => props.setCategory('category=' + item.value)}>{item.title}</Btn></div>
                         )
                     })
                 }
             </div>
             <div className={`text-3xl`}>Select Difficulty</div>
             <div className={`${styles.difficultyContainer} flex justify-center overflow-hidden mx-auto mb-4`}>
-                    <div><Btn onClick={() => props.setDifficulty('difficulty=easy')}>Easy</Btn></div>
-                    <div><Btn onClick={() => props.setDifficulty('difficulty=medium')}>Normal</Btn></div>
-                    <div><Btn onClick={() => props.setDifficulty('difficulty=hard')}>Hard</Btn></div>
+                    <div><Btn customTW="text-sm" onClick={() => props.setDifficulty('difficulty=easy')}>Easy</Btn></div>
+                    <div><Btn customTW="text-sm" onClick={() => props.setDifficulty('difficulty=medium')}>Normal</Btn></div>
+                    <div><Btn customTW="text-sm" onClick={() => props.setDifficulty('difficulty=hard')}>Hard</Btn></div>
             </div>
         </div>
     )
