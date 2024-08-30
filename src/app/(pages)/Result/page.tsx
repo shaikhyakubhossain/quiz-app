f1eb9bae9a3776cb917376d9741d7b3c1baba32e
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-30 16:02:56 +0530

    Modify Result page

--- src/app/(pages)/Result/page.tsx
+++ src/app/(pages)/Result/page.tsx
@@ -4,7 +4,7 @@ import DisplayScore from '../../component/DisplayScore/display-score.component'
 export default function Result(): JSX.Element {
     return (
         <div className={`w-auto flex flex-col justify-center text-center`}>
-            <div className="text-5xl text-center mb-12">Thank you for testing</div>
+            <div className="text-5xl text-center mb-12">Thank you for playing</div>
             <DisplayScore />
         </div>
     ) 
